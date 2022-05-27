b74ff85f8977478fc6320db6fba2d1c99ec5bb21
joskalenda <joskalenda3@gmail.com>
2022-05-27 14:41:37 +0200

    Add unit testing for entity model

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,4 +2,8 @@ class Entity < ApplicationRecord
   belongs_to :user
   has_many :category_entities, dependent: :destroy
   has_many :categories, through: :category_entities
+
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { only_integer: true }
+
 end
